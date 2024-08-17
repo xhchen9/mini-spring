f5903f8ca2473011c3170d321617311807f9503c
victus <2655435301@qq.com>
2024-08-17 17:37:00 +0800

    feat: 实现简单的BeanFactory以及BeanDefinition

---
+++ src/main/java/com/cxh/springframework/BeanFactory.java
@@ -0,0 +1,17 @@
+package com.cxh.springframework;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class BeanFactory {
+
+    private Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>();
+
+    public Object getBean(String name){
+        return  beanDefinitionMap.get(name).getBean();
+    }
+
+    public void registerBeanDefinition(String name, BeanDefinition beanDefinition){
+        beanDefinitionMap.put(name, beanDefinition);
+    }
+}
