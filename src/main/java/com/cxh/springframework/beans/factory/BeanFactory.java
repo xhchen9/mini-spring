6dddf41bf86a7b0d0865611fae504cb4206101a6
victus <2655435301@qq.com>
2024-09-04 21:19:51 +0800

    feat: 可传入构造函数参数 to 基于Cglib实现含构造函数的类实例化策略

--- src/main/java/com/cxh/springframework/beans/factory/BeanFactory.java
+++ src/main/java/com/cxh/springframework/beans/factory/BeanFactory.java
@@ -1,9 +1,14 @@
 package com.cxh.springframework.beans.factory;
 
+import com.cxh.springframework.beans.BeansException;
+
 /**
  * 负责bean的获取
  */
 public interface BeanFactory {
 
-   Object getBean(String beanName);
+   Object getBean(String beanName) throws BeansException;
+
+   // 实例化对象时，可传入参数args，调用有参构造
+   Object getBean(String beanName, Object... args) throws BeansException;
 }
