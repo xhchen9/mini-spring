c4398701a302f27aab6cd1b39f0a7b09915b8efe
victus <2655435301@qq.com>
2024-09-04 21:23:17 +0800

    feat: 增加有参构造实例化方式 to 基于Cglib实现含构造函数的类实例化策略

--- src/main/java/com/cxh/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/com/cxh/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -3,19 +3,48 @@ package com.cxh.springframework.beans.factory.support;
 import com.cxh.springframework.beans.BeansException;
 import com.cxh.springframework.beans.factory.config.BeanDefinition;
 
+import java.lang.reflect.Constructor;
+
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
+    // 创建对象的实例化策略属性类
+    private InstantiationStrategy instantiationStrategy = new CglibSubclassingInstantiationStrategy();
 
     @Override
-    protected Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
+    protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) throws BeansException {
         Object bean = null;
 
         try{
-            bean = beanDefinition.getBeanClass().newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
+            bean = createBeanInstance(beanDefinition, beanName, args);
+        } catch (Exception e) {
             throw new BeansException("Instantiation of bean failed", e);
         }
         // 将bean添加至父类map
         addSingleton(beanName, bean);
         return bean;
+    }
+
+    protected Object createBeanInstance(BeanDefinition beanDefinition, String beanName, Object[] args){
+        Constructor constructor = null;
+
+        Class<?> beanClass = beanDefinition.getBeanClass();
+        Constructor<?>[] declaredConstructors = beanClass.getDeclaredConstructors();
+
+        for (Constructor ctor : declaredConstructors){
+            // 根据构造函数参数数目选择构造函数（简单化了）
+            if(args != null && ctor.getParameterTypes().length == args.length){
+                constructor = ctor;
+                break;
+            }
+        }
+
+        return getInstantiationStrategy().instantiate(beanDefinition, beanName, constructor, args);
+    }
+
+    public InstantiationStrategy getInstantiationStrategy() {
+        return instantiationStrategy;
+    }
+
+    public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
+        this.instantiationStrategy = instantiationStrategy;
     }
 }
